ada0b2e44b3a40aa3ee6323f0956d173a3af8122
Ross O'Reilly <oreillyross@gmail.com>
2019-07-19 11:46:12 +0000

    add up timer

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var counter = 0
+
+const count = () => {
+    counter++
+}
+
+setInterval(count, 1000)
+
+app.get('/', (req, res) => res.send(`Hello World!, ${counter}`))
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
