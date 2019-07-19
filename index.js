05fd678e2cdc6a8610cefb4df6c667a66f47f6a2
Ross O'Reilly <oreillyross@gmail.com>
2019-07-19 13:54:35 +0000

    now json

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const app = express()
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 80
 
+require('dotenv').config()
 
 
+app.use(logger('dev'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-   res.send(`Hello World!, counter gone`)
-})
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
 
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
 
 
