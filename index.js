d49b299aca5ee335789f0c443a106745afee599e
Ross O'Reilly <oreillyross@gmail.com>
2019-07-26 08:53:02 +0000

    comon heroku

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
+const  { ApolloClient }  = require("apollo-boost");
+const gql = require("graphql-tag");
+require('cross-fetch/polyfill');
+
+const { createHttpLink } = require("apollo-link-http");
+const { InMemoryCache } = require('apollo-cache-inmemory');
+
+const link = createHttpLink({ uri: "https://horizon-hasura.herokuapp.com/v1/graphql" });
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+});
+
+const GET_AUTOMATEDSEARCH_ON = gql`
+  query getConfig {
+  config_by_pk(id: "12345") {
+    id
+    automatedSearchOn
+  }
+}
+`
+
+client.query({
+  query: GET_AUTOMATEDSEARCH_ON,
+  
+})
+  .then(result => console.log(result));
+
+
+
 const port = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const app = express();
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: 'World'}));
+});
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
