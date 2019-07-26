764cb705f3b89726c469c8ed047d875db36d5453
Ross O'Reilly <oreillyross@gmail.com>
2019-07-26 09:03:05 +0000

    add graphql

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ const app = express();
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
+
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
