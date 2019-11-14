a09367c276268fd9798c2ff0cf9d7be4fb3e1d1b
Faktor 10 <oreillyross@gmail.com>
2019-11-14 16:36:19 +0100

    ssds

---
+++ src/trawl.py
@@ -0,0 +1,10 @@
+# create a script which one can run every two hours using a cron job.
+# or look at creating a serverless function again which can be run by serverless
+# function.
+
+# function which records begin time
+# timesout after a set time (5secs or such)
+# fetches articles from a news source based on keywords
+# saves the metadata of articles on postgres database
+# saves the text of article (strip images and hyperlinks, etc)
+# in postgres database
