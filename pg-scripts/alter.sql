4845a38ef78141a340f7a717f81d90a577b17b48
Ross O'Reilly <oreillyross@gmail.com>
2020-01-07 13:05:03 +0000

    add more scripts

---
+++ pg-scripts/alter.sql
@@ -0,0 +1,3 @@
+--ALTER TABLE sources DROP COLUMN created_on SET DEFAULT NOW()
+
+ALTER TABLE sources DROP COLUMN updated_on
---
+++ pg-scripts/seed.sql
@@ -0,0 +1,1 @@
+INSERT INTO SOURCES (source_id, name, url) VALUES (1, 'Naharnet Lebanon News', 'http://www.naharnet.com/lebanon')
