b85804cd2cb554597c3d358bb1be2ae82d16af5b
Ross O'Reilly <oreillyross@gmail.com>
2020-02-05 11:43:23 +0000

    update

--- pg-scripts/create-scripts.sql
+++ pg-scripts/create-scripts.sql
@@ -1,11 +1,11 @@
-/*
+
 CREATE TABLE articles (
    article_id serial PRIMARY KEY,
    title VARCHAR (200) UNIQUE NOT NULL,
    article TEXT NOT NULL,
    trawled_on TIMESTAMP NOT NULL
 );
-*/
+
 
 CREATE TABLE sources (
    source_id serial PRIMARY KEY,
